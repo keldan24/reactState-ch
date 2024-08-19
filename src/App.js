3de400ec5d4dca43a2c7298e9f5aa4d1d35ab3f4
keldan24 <kelechidaniel171kd@gmail.com>
2024-08-19 08:17:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize the state
+    this.state = {
+      person: {
+        fullName: 'John Caesar',
+        bio: 'A passionate software developer.',
+        imgSrc: '/portfolio.png',
+        profession: 'Software Developer'
+      },
+      show: false,
+      timeSinceMount: 0
+    };
+
+    // Bind the toggle function
+    this.toggleShow = this.toggleShow.bind(this);
+  }
+
+  // Lifecycle method to set up the interval
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({
+        timeSinceMount: Math.floor((new Date() - this.mountTime) / 1000)
+      }));
+    }, 1000);
+
+    // Record the mount time
+    this.mountTime = new Date();
+  }
+
+  // Lifecycle method to clear the interval
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  // Toggle the show state
+  toggleShow() {
+    this.setState((prevState) => ({
+      show: !prevState.show
+    }));
+  }
+
+  render() {
+    const { person, show, timeSinceMount } = this.state;
+
+    return (
+      <div className="App">
+        <button onClick={this.toggleShow}>
+          {show ? 'Hide Profile' : 'Show Profile'}
+        </button>
+        {show && (
+          <div className="profile">
+            <img src={person.imgSrc} alt={person.fullName} />
+            <h1>{person.fullName}</h1>
+            <p>{person.bio}</p>
+            <p><strong>Profession:</strong> {person.profession}</p>
+          </div>
+        )}
+        <p>Time since component mounted: {timeSinceMount} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
